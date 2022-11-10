0809fde1c85b16c63f70b4a551c483eb07148618
Cody Cooper <cody_codster@live.com>
2022-11-10 01:12:02 -0600

    Added Bootstrap, JQuery, and dayJs to HTML file

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Weather API Doc: https://openweathermap.org/forecast5
+//Weather API key: 2e4652b7589b2f1ce92a963bd9db3bfa
+//5-day Weather Forecast URL: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+//Direct Geocoding: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+//Use Direct Geocoding to grab latitude and longitude to plug into 5-day Weather-forcast request.
