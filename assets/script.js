420d35b27d67f0b4fee6795ebabebe929aa6cc97
Cody Cooper <cody_codster@live.com>
2022-11-12 18:39:12 -0600

    Updated HTML structure

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,19 @@
-var currentDate = dayjs().format('MMM D, YYYY');
-
+//Collects current date
+var currentDate = dayjs().format('dddd MMM D, YYYY');
 $('#currentTime').text(currentDate);
 
+//Weather APIs
+var Apikey = '2e4652b7589b2f1ce92a963bd9db3bfa'
+var geoUrl = 'https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=2e4652b7589b2f1ce92a963bd9db3bfa'
+var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=36.3724&lon=94.2102&units=imperial&appid=2e4652b7589b2f1ce92a963bd9db3bfa'
 
+function getApi() {
+    fetch(requestUrl) 
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+};
 
-
-
-
-
-
-
-
-
-
-
-
+getApi();
 
 
 
